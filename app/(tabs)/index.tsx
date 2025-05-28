c15dc5189f3d33d267840d8b0847ee023607c13c
Yessboom <Tanguy.guissard@hotmail.be>
2025-05-28 17:26:19 +0200

    Tutorial-BuildAScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
+
+
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-      Go to about Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+        
+        </View>ù<View style={styles.footerContainer}>
+          <Button label="Choose a photo" theme="primary"/>
+          <Button label="Use this photo" />
+          </View>
     </View>
   );
 }
@@ -26,5 +34,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff'
-  }
+  }, 
+  imageContainer:{
+    flex:1
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 });
